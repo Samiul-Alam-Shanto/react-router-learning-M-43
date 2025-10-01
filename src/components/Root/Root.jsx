c38deb6d6d2af18fad9b6aab63b8803d917b70df
Samiul-Alam-Shanto <samiulalamshantowpdev@gmail.com>
2025-10-01 19:29:10 +0600

    learned the loading or pending UI using the useNavigation and boolean and used it in main.jsx

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import Header from "../Header/Header";
-import { Outlet } from "react-router";
+import { Outlet, useNavigation } from "react-router";
 import Footer from "../Footer/Footer";
 import SideBar from "../SideBar/SideBar";
 
 const Root = () => {
+  const navigation = useNavigation();
+  const isNavigating = Boolean(navigation.location);
   return (
     <div>
       <Header></Header>
       <div className="flex items-center gap-10">
         <SideBar></SideBar>
+        {isNavigating && <span>'Loading....'</span>}
         <Outlet></Outlet>
       </div>
       <Footer></Footer>
