5939714ffdda6f3515c4e7aa9d7e834fe40d29eb
Samiul-Alam-Shanto <samiulalamshantowpdev@gmail.com>
2025-10-01 19:32:49 +0600

    used the useParam hook in users

--- src/components/UserDetails/UserDetails.jsx
+++ src/components/UserDetails/UserDetails.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { useLoaderData } from "react-router";
+import { useLoaderData, useParams } from "react-router";
 
 const UserDetails = () => {
   const userInfo = useLoaderData();
-  console.log(userInfo);
+  // console.log(userInfo);
+
+  // const params = useParams();
+  // console.log(params);
+  // console.log(params.userId);
+  const { userId } = useParams();
   return (
     <div>
+      <h2>{userId}</h2>
       <h3>Name: {userInfo.name}</h3>
       <h5>Website: {userInfo.website}</h5>
     </div>
