c8d7f7e9554babc8ce059f78c3dfdfda4365c8fb
Samiul-Alam-Shanto <samiulalamshantowpdev@gmail.com>
2025-10-01 19:18:26 +0600

    used the useNavigate to handle event instead of using link and used it in posts

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,16 +2,32 @@ import React from "react";
 import { NavLink } from "react-router";
 
 const Header = () => {
+  const navStyle = ({ isActive }) =>
+    ` ${
+      isActive ? "text-blue-600 underline font-bold" : " hover:text-blue-400"
+    }`;
   return (
     <div>
       <h3>This is Header</h3>
       <nav className="flex gap-4">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/mobiles">Mobiles</NavLink>
-        <NavLink to="/laptops">Laptops</NavLink>
-        <NavLink to="/users">Users</NavLink>
-        <NavLink to="/users2">Users2</NavLink>
-        <NavLink to="/posts">Posts</NavLink>
+        <NavLink to="/" className={navStyle}>
+          Home
+        </NavLink>
+        <NavLink className={navStyle} to="/mobiles">
+          Mobiles
+        </NavLink>
+        <NavLink className={navStyle} to="/laptops">
+          Laptops
+        </NavLink>
+        <NavLink className={navStyle} to="/users">
+          Users
+        </NavLink>
+        <NavLink className={navStyle} to="/users2">
+          Users2
+        </NavLink>
+        <NavLink className={navStyle} to="/posts">
+          Posts
+        </NavLink>
       </nav>
       {/* navlink dile extra ekta class dynamically add kore dey */}
     </div>
--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,14 +1,22 @@
 import React from "react";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 
 const Post = ({ post }) => {
   const { id, title } = post;
+
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate(`/posts/${id}`);
+  };
+
   return (
     <div className="border mt-2 rounded-xl">
       <h4>{title}</h4>
       <Link to={`/posts/${id}`}>
         <button className="mt-2">Show Details</button>
       </Link>
+      <button onClick={handleNavigate}>Details of {id}</button>
     </div>
   );
 };
